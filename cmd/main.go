b93694d5413845c3a663a8f608fd4eb248631d95
David Gray <54521366+davidgraymi@users.noreply.github.com>
2025-07-22 12:32:02 -0500

    change logo and update deployed licenses (#65)

--- cmd/main.go
+++ cmd/main.go
@@ -120,7 +120,7 @@ type AppVersion struct {
 func NewMainApp(appVer AppVersion) *cli.App {
 	app := cli.NewApp()
 	app.Name = "Bindersnap"
-	app.HelpName = "bsnap"
+	app.HelpName = "gitea"
 	app.Usage = "A painless, fully managed document service"
 	app.Description = `Bindersnap program contains "web" and other subcommands. If no subcommand is given, it starts the web server by default. Use "web" subcommand for more web server arguments, use other subcommands for other purposes.`
 	app.Version = appVer.Version + appVer.Extra
